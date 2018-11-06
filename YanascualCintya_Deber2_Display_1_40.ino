1711016c0868f9320f2fcc92b150fadbbfaebfd7
Cintya02 <44457108+Cintya02@users.noreply.github.com>
2018-11-06 00:53:37 -0500

    Add files via upload

---
+++ YanascualCintya_Deber2_Display_1_40.ino
@@ -0,0 +1,160 @@
+/*
+            UNIVERSIDAD TÉCNICA DEL NORTE
+                    FICA-CIERCOM
+              SISTEMAS MICROPROCESADOS
+   Nombre: Cintya Yanascual
+   Fehca: 06/11/2018
+
+*/
+
+//CONTADOR 1 AL 40 ASCENDENTE Y DESCENDENTE
+
+int A = 5;
+int B = 4;
+int C = 3;
+int D = 2;
+int b1 = 6;
+int b2=7;
+int i = 0;
+int dec = 9;
+int uni = 8;
+int unidades;
+int decenas;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(b1, INPUT);
+  pinMode(b2, OUTPUT);
+  pinMode(dec, OUTPUT);
+  pinMode(uni, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (digitalRead(b1) == HIGH) {
+    delay(200);
+    if (i <= 40){
+      i++;
+    decenas = i / 10;
+    unidades = i - (decenas * 10);
+
+  } else {
+    i = 0;
+    unidades =0;
+    decenas =0;
+    
+  }
+  }
+  
+if (digitalRead(b2) == HIGH) {
+    delay(200);
+    if (i <= 40){
+      i--;
+    decenas = i / 10;
+    unidades = i - (decenas * 10);
+
+  } else {
+    i = 0;
+    unidades =0;
+    decenas =0;
+    
+  }
+}
+digitalWrite(uni, HIGH);
+digitalWrite(dec, LOW);
+displaycont(unidades);
+delay(100);
+
+digitalWrite(uni, LOW);
+digitalWrite(dec, HIGH);
+displaycont(decenas);
+delay(100);
+
+}
+void displaycont(int j) {
+  switch (j) {
+    //0
+    case 0:
+      digitalWrite(A, LOW);
+      digitalWrite(B, LOW);
+      digitalWrite(C, LOW);
+      digitalWrite(D, LOW);
+      break;
+
+    //1
+    case 1:
+      digitalWrite(A, HIGH);
+      digitalWrite(B, LOW);
+      digitalWrite(C, LOW);
+      digitalWrite(D, LOW);
+      break;
+
+    //2
+    case 2:
+      digitalWrite(A, LOW);
+      digitalWrite(B, HIGH);
+      digitalWrite(C, LOW);
+      digitalWrite(D, LOW);
+      break;
+
+    //3
+    case 3:
+      digitalWrite(A, HIGH);
+      digitalWrite(B, HIGH);
+      digitalWrite(C, LOW);
+      digitalWrite(D, LOW);
+      break;
+
+    //4
+    case 4:
+      digitalWrite(A, LOW);
+      digitalWrite(B, LOW);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, LOW);
+      delay(300);
+
+    //5
+    case 5:
+      digitalWrite(A, HIGH);
+      digitalWrite(B, LOW);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, LOW);
+      break;
+
+    //6
+    case 6:
+      digitalWrite(A, LOW);
+      digitalWrite(B, HIGH);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, LOW);
+      break;
+
+    //7
+    case 7:
+      digitalWrite(A, HIGH);
+      digitalWrite(B, HIGH);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, LOW);
+      break;
+
+    //8
+    case 8:
+      digitalWrite(A, LOW);
+      digitalWrite(B, LOW);
+      digitalWrite(C, LOW);
+      digitalWrite(D, HIGH);
+      break;
+
+    //9
+    case 9:
+      digitalWrite(A, HIGH);
+      digitalWrite(B, LOW);
+      digitalWrite(C, LOW);
+      digitalWrite(D, HIGH);
+      break;
+  }
+}
